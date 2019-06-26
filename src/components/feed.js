0a71f4f1ad83b54e20b879e6b00f72999888f752
James Pickering <james.pickering@gmail.com>
2019-06-26 23:30:35 +0100

    card UI tweaks

--- src/components/feed.js
+++ src/components/feed.js
@@ -14,21 +14,17 @@ export default class Feed extends Component {
     );
     const feed = filtered.map(item => {
       return (
-        <div key={item.id} className="container feed">
-          <div className="row">
+        <div className="card feed" key={item.id}>
+          <div className="card-header">
             <h3> {item.title} </h3>
           </div>
-          <div className="row">
+          <div className="card-body">
             <p> {item.body} </p>
-          </div>
-          <div className="row">
             <p>
-              <span className="badge badge-light"> {item.badge} </span>{" "}
-              <span className="credit"> {item.category} </span>{" "}
-              <span className="credit">
-                {" "}
-                {item.author} {item.date}{" "}
-              </span>
+              <span className="badge badge-light">{item.badge}</span>
+              <span className="credit"> {item.category} </span>
+              <span className="credit">{item.author}</span>
+              <span className="credit">{item.date}</span>
             </p>
           </div>
         </div>
