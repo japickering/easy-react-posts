9229ab3c36edb9ca45097dd7c9c1fafe09b7fea6
James Pickering <james.pickering@gmail.com>
2019-06-28 11:00:19 +0100

    pagination rewrite

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,40 +1,29 @@
 import React, { Component } from "react";
-import data from "../data.json";
 
 export default class Pagination extends Component {
   render() {
-    const { search, onChildDateChange } = this.props;
-    const filtered = data.filter(
-      item =>
-        item.title === search ||
-        item.year === search ||
-        item.author.indexOf(search) !== -1 ||
-        item.badge.indexOf(search) !== -1 ||
-        item.body.indexOf(search) !== -1
-    );
-    // setPages = totalPosts / postsPerPage
-    const postsPerPage = 5;
-    const totalPosts = filtered.length;
-    const pages = Math.ceil(totalPosts / postsPerPage);
-    console.log("pages: " + pages);
-
-    let list = [];
-    for (let i = 1; i <= pages; i++) {
-      list.push(
-        <a className='page-link' href='#'>
-          {i}
-        </a>
-      );
+    const { totalPosts, postsPerPage, paginate } = this.props;
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+      pageNumbers.push(i);
     }
 
     return (
-      <div>
-        {totalPosts >= postsPerPage && (
-          <nav aria-label='Page navigation'>
-            <ul className='pagination'>{list}</ul>
-          </nav>
-        )}
-      </div>
+      <nav>
+        <ul className='pagination'>
+          {pageNumbers.map(number => (
+            <li key={number} className='page-item'>
+              <a
+                onClick={() => paginate(number)}
+                href='!#'
+                className='page-link'
+              >
+                {number}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     );
   }
 }
