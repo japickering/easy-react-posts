373885d4004dd9812e05574cf95742f238415061
James Pickering <james.pickering@gmail.com>
2019-07-02 13:02:47 +0100

    fixed a typo

--- src/App.test.js
+++ src/App.test.js
@@ -20,7 +20,6 @@ it("renders without crashing", () => {
   ReactDOM.render(<Posts />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-});
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Pagination />, div);
